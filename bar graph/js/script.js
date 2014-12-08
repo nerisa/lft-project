70e65adaa66ca958c864a914684d25057daa9a33
nerisa <chitrakarnerisa@gmail.com>
2014-12-08 21:58:10 +0545

    added labels

--- bar graph/js/script.js
+++ bar graph/js/script.js
@@ -5,17 +5,18 @@ function BarGraph(id,properties){
 	this.data=properties.data;
 	this.colors=properties.colors;
 	this.label=properties.label;
-	this.barWidth=10;
-	this.barLength=50;
+	this.labelX=properties.labelX;
+	this.labelY=properties.labelY;
+	this.barWidth=30;
+	this.barLength=40;
 	var that=this;
-	
-	
-	this.drawOneBar=function(context,canvas,data,barTop,barHeight){
+		
+	this.drawOneBar=function(context,data,barTop,barHeight,color){
 		var x=barTop.x;
 		var y=barTop.y;
-		console.log(y,barHeight);
+		//console.log(y,barHeight);
 		context.save();
-		context.fillStyle="#ff0000";
+		context.fillStyle=color;
 		context.fillRect(x,y,that.barWidth,barHeight);
 		context.restore();
 	}
@@ -23,29 +24,81 @@ function BarGraph(id,properties){
 	this.draw=function(){
 		var context=that.canvas.getContext('2d');
 		var graphLength=that.data.length*that.barLength;
-		var graphHeight=350;
+		var canvasWidth=graphLength+100;
+		that.canvas.width=canvasWidth;
+	
+		var unitHeight=10;
+		var graphHeight=(Math.max.apply(Math,that.data))*unitHeight;
+		var canvasHeight=graphHeight+100;
+		console.log(canvasHeight);
+		that.canvas.height=canvasHeight;
+		console.log(graphHeight);
+		var startX=40;
+		var startY=50;
+		
+		
+		//draw graph axes
 		context.beginPath();
-		context.moveTo(10,10);
-		context.lineTo(10,graphHeight);
-		context.lineTo(graphLength,graphHeight);
+		context.moveTo(startX,startY);
+		context.lineTo(startX,startY+graphHeight);
+		context.lineTo(graphLength,startY+graphHeight);
 		context.lineJoin='miter';
 		context.stroke();
 		
-		var spaceBetweenBars=(that.barLength-that.barWidth)/2;
+		//var spaceBetweenBars=40;
 		var dataSum=that.sumOfData(that.data);
-		var unitHeight=Math.floor(graphHeight/dataSum);
+		//var unitHeight=Math.floor(graphHeight/dataSum);
+		
 		
 		for(var i=0;i<that.data.length;i++){
 			var barHeight=that.data[i]*unitHeight;
 			var barStart={
-							x:(i*that.barLength+spaceBetweenBars),
-							y:(graphHeight-barHeight)
+							x:(startX+(i*that.barWidth)+i),
+							y:startY+(graphHeight-barHeight)
+						};
+			console.log(barStart.y);
+			var labelPos={
+							x:barStart.x,
+							y:startY+graphHeight+10
+						};
+			var dataPos={
+							x:barStart.x+5,
+							y:barStart.y
+						};
+			var xAxes={
+							x:((startX+graphLength)/2)-50,
+							y:labelPos.y+10
 						};
 			
 			//console.log(barStart.x,barStart.y);
-			that.drawOneBar(context,that.canvas,that.data[i],barStart,barHeight);
+			that.drawOneBar(context,that.data[i],barStart,barHeight,that.colors[i]);
+			that.writeLabels(context,that.label[i],that.data[i],labelPos,dataPos);
 		
 		}
+		that.writeAxes(context,xAxes,30);
+		
+	}
+	
+	this.writeLabels=function(context,label,data,labelPos,dataPos){
+		context.save();
+		context.textAlign="left";
+		context.fillStyle="#000";
+		context.fillText(label,labelPos.x,labelPos.y,200);
+		
+		context.textAlign="centre";
+	
+		context.fillText(data,dataPos.x,dataPos.y,200);
+		context.restore();
+	}
+	
+	this.writeAxes=function(context,xPos,yPos){
+		context.save();
+		context.fillStyle="#F00"
+		context.fillText(that.labelX,xPos.x,xPos.y);
+		
+		context.rotate(Math.PI);
+		context.fillText("hello",100,0,200);
+		context.restore();
 	}
 	
 	
@@ -65,8 +118,10 @@ function BarGraph(id,properties){
 var barGraph=new BarGraph("bargraph",
 						  {
 							  data:[5,6,10,2,6,3,2],
-							  colors:["#ff000000","#ff000000","#ff000000","#ff0000","#ff0000","#ff0000","#ff0000"],
-							  label:["2005","2006","2007","2008","2006","2007","2008"]
+							  colors:["#ff0045","#ff6700","#fffb00","#ff0230","#ff0080","#ffbb00","#ff0aa0"],
+							  label:["2005","2006","2007","2008","2006","2007","2008"],
+							  labelX:"Years",
+							  labelY:"No. of persons"
 						  }
 						  );
 barGraph.draw();
