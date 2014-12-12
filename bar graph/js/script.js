b21e2be06aa5c21618d0cc246ff79b067dbe15e9
nerisa <chitrakarnerisa@gmail.com>
2014-12-12 11:01:10 +0545

    dynamic+grid+label

--- bar graph/js/script.js
+++ bar graph/js/script.js
@@ -3,15 +3,48 @@
 function BarGraph(id,properties){
 	this.canvas=document.getElementById(id);
 	this.data=properties.data;
-	this.colors=properties.colors;
+	this.colors="rgba(0,255,0,0.8)";
 	this.label=properties.label;
 	this.labelX=properties.labelX;
 	this.labelY=properties.labelY;
-	this.barWidth=30;
-	this.barLength=40;
+	this.barWidth
+	this.margin=40;
+	this.yStep;
+	this.xStep;
+	this.yDivisions=10;
+	this.yIncrement;
+	this.graphHeight;
+	this.graphLength;
+	this.unitHeight;
+	this.snapShot;
+	var context=this.canvas.getContext('2d');
 	var that=this;
+	
+	this.init=function(){
+		that.graphLength=that.canvas.width-2*that.margin;
+		that.xStep=that.graphLength/that.data.length;
+		console.log(that.xStep);
+		that.graphHeight=that.canvas.height-2*that.margin;
+		var maxVal=Math.max.apply(Math,that.data);
+		var minVal=Math.min.apply(Math,that.data);
+		that.yStep=that.graphHeight/that.yDivisions;
+		that.yIncrement=Math.ceil((maxVal-minVal)/that.yDivisions);
+		that.barWidth=that.xStep/2;
+		console.log(that.barWidth);
+		that.drawAxes();
+		that.drawGrid();
+		that.writeAxes();
+		//var fontSize=Math.floor(that.newScale);
+			
+		context.save();
 		
-	this.drawOneBar=function(context,data,barTop,barHeight,color){
+		that.draw();
+		context.restore();
+		window.addEventListener('mousedown', that.enableData, false);
+		window.addEventListener("mouseup",that.disableData,false);
+	}
+		
+	this.drawOneBar=function(barTop,barHeight,color){
 		var x=barTop.x;
 		var y=barTop.y;
 		//console.log(y,barHeight);
@@ -22,97 +55,106 @@ function BarGraph(id,properties){
 	}
 	
 	this.draw=function(){
-		var context=that.canvas.getContext('2d');
-		var graphLength=that.data.length*that.barLength;
-		var canvasWidth=graphLength+100;
-		that.canvas.width=canvasWidth;
-	
-		var unitHeight=10;
-		var graphHeight=(Math.max.apply(Math,that.data))*unitHeight;
-		var canvasHeight=graphHeight+100;
-		console.log(canvasHeight);
-		that.canvas.height=canvasHeight;
-		console.log(graphHeight);
-		var startX=40;
-		var startY=50;
+		context.translate(that.margin,that.canvas.height-that.margin);
+		var newScale=that.yStep/that.yIncrement;
+		context.scale(1,-1*newScale);
+		for(var i=0;i<that.data.length;i++){
+			var barHeight=that.data[i];
+			var barStart={
+							x:that.xStep/4+i*that.xStep,
+							y:0
+						};
+			that.drawOneBar(barStart,barHeight,that.colors);
+		}
 		
-		
-		//draw graph axes
+	}
+	this.drawAxes=function(){
+		context.strokeStyle="#000000";
 		context.beginPath();
-		context.moveTo(startX,startY);
-		context.lineTo(startX,startY+graphHeight);
-		context.lineTo(graphLength,startY+graphHeight);
+		context.moveTo(that.margin,that.margin);
+		context.lineTo(that.margin,that.canvas.height-that.margin);
+		context.lineTo(that.canvas.width-that.margin,that.canvas.height-that.margin);
 		context.lineJoin='miter';
 		context.stroke();
-		
-		//var spaceBetweenBars=40;
-		var dataSum=that.sumOfData(that.data);
-		//var unitHeight=Math.floor(graphHeight/dataSum);
-		
-		
-		for(var i=0;i<that.data.length;i++){
-			var barHeight=that.data[i]*unitHeight;
-			var barStart={
-							x:(startX+(i*that.barWidth)+i),
-							y:startY+(graphHeight-barHeight)
-						};
-			console.log(barStart.y);
-			var labelPos={
-							x:barStart.x,
-							y:startY+graphHeight+10
-						};
-			var dataPos={
-							x:barStart.x+5,
-							y:barStart.y
-						};
-			var xAxes={
-							x:((startX+graphLength)/2)-50,
-							y:labelPos.y+10
-						};
-			
-			//console.log(barStart.x,barStart.y);
-			that.drawOneBar(context,that.data[i],barStart,barHeight,that.colors[i]);
-			that.writeLabels(context,that.label[i],that.data[i],labelPos,dataPos);
-		
+	}
+	
+	this.drawGrid=function(){
+		//horizontal grids
+		context.strokeStyle="#cccccc";
+		context.beginPath();
+		for(var i=1;i<=that.yDivisions;i++){
+			var y=that.canvas.height-that.margin-i*that.yStep;
+			context.moveTo(that.margin,y);
+			context.lineTo(that.canvas.width-that.margin,y);
+			var scale=i*that.yIncrement;
+			context.fillText(scale,that.margin/2,y);
 		}
-		that.writeAxes(context,xAxes,30);
+		context.stroke();
 		
+		//x divisions
+		context.strokeStyle="#000000";
+		context.beginPath();
+		var x;
+		for(var i=1;i<that.data.length;i++){
+			x=that.margin+i*that.xStep;
+			context.moveTo(x,that.canvas.height-that.margin);
+			context.lineTo(x,that.canvas.height-that.margin+10);
+			var textPos=x-that.xStep/2-that.label[i-1].length/2
+			context.fillText(that.label[i-1],textPos,that.canvas.height-that.margin+10,50);
+		}
+		context.stroke();
+		var textPos=x+that.xStep/2-that.label[that.data.length-1].length/2
+		context.fillText(that.label[that.data.length-1],textPos,that.canvas.height-that.margin+10,50);
 	}
 	
-	this.writeLabels=function(context,label,data,labelPos,dataPos){
-		context.save();
-		context.textAlign="left";
-		context.fillStyle="#000";
-		context.fillText(label,labelPos.x,labelPos.y,200);
-		
-		context.textAlign="centre";
 	
-		context.fillText(data,dataPos.x,dataPos.y,200);
-		context.restore();
-	}
-	
-	this.writeAxes=function(context,xPos,yPos){
-		context.save();
+	this.writeAxes=function(){
+		var xPos={
+					x:that.margin+that.graphLength/2-that.labelX.length/2,
+					y:that.canvas.height-that.margin/4
+		}
 		context.fillStyle="#F00"
 		context.fillText(that.labelX,xPos.x,xPos.y);
 		
-		context.rotate(Math.PI);
-		context.fillText("hello",100,0,200);
-		context.restore();
+		//context.rotate(-Math.PI/2);
+		//context.fillText("hello",100,0,200);
 	}
 	
+	this.getMousePos=function(canvas, evt) {
+    	var rect = canvas.getBoundingClientRect();
+    	return {
+      		x: evt.clientX - rect.left,
+      		y: evt.clientY - rect.top
+    	};
+	}
 	
-	
-	this.sumOfData=function(data){
-		var sum=0;
-		//console.log(data.length);
-		for(var i=0;i<data.length;i++){
-			//console.log(data[i]);
-			sum+=data[i];
+	this.enableData=function(e){
+    	var pos = that.getMousePos(that.canvas, e);
+    	var posx = (pos.x-that.margin);
+    	var posy = (that.canvas.height-that.margin-pos.y)/that.newScale;
+		var graphPosX=(posx/that.xStep);
+		var dataNo=Math.floor(posx/that.xStep);
+		console.log(dataNo);
+		var graphPosY=Math.round(posy);
+		var text="(" + that.label[dataNo] + "," + that.data[dataNo] + ")";
+		var textPos={
+							x:posx,
+							y:that.data[dataNo]
+					}
+		that.snapShot=context.getImageData(0,0,that.canvas.width,that.canvas.height);
+		context.fillStyle="#006600"
+		var fontSize=Math.floor(that.canvas.height/30);
+		context.font=fontSize+"pt Helvetica";
+		console.log(text);
+		context.fillText(text,textPos.x,textPos.y);
 		}
-		return sum;
-	}
 		
+	
+	this.disableData=function(){
+		context.clearRect(0,0,that.canvas.width,that.canvas.height);
+		context.putImageData(that.snapShot,0,0);
+	}
+	
 }
 
 var barGraph=new BarGraph("bargraph",
@@ -124,7 +166,7 @@ var barGraph=new BarGraph("bargraph",
 							  labelY:"No. of persons"
 						  }
 						  );
-barGraph.draw();
+barGraph.init();
 						  
 
 	
